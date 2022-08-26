422b47431d28be9c6c87d52e90c68f3954217df4
vedprakashjangid <jangid.ved44@gmail.com>
2022-08-25 21:12:35 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+
+// import { useEffect,useState} from 'react';
+import Todo from "./Todo"
+// import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+   return <Todo/>
+//  const [users,setUsers]=useState([]);
+
+//  const getApi= async ()=>{
+//   //  let response =await fetch("https://jsonplaceholder.typicode.com/users");
+//    let response =await fetch("https://jsonplaceholder.typicode.com/posts");
+//   setUsers( await response.json());
+//  }
+//  useEffect(()=>{
+//    getApi();
+//  },[]);
+ 
+
+//   return (
+//    <> <h1>API Calling</h1>
+//    <div className="container">
+
+
+//      {
+//       users.map((user)=>(
+
+//         <div className="box">
+//       {/* <div>Name:<span>{user.name}</span></div>
+//       <div>Email:<span>{user.email}</span></div> */}
+
+//       <div>Title:<span>{user.title.substr(0,10)}</span><span><a href='#'> see more...</a></span></div>
+//       <div>Body:<span>{user.body.substr(0,20)}</span><span>...</span></div>
+
+//     </div>
+//       ) )
+        
+        
+//      }
+//     {/* <div className="box">
+//       <div>Name:<span>ved</span></div>
+//       <div>Email:<span>ved@gmail.com</span></div>
+
+//     </div> */}
+    
+//    </div>
+//    </>
+//   );
 }
 
 export default App;
